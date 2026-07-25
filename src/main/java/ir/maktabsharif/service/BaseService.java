de3a5960bc3e1e387810ec9a3412a76f4bd6f100
Amiralimhrngr <ftmr92@gmail.com>
2026-07-26 01:49:47 +0330

    feat(service): create BaseService

---
+++ src/main/java/ir/maktabsharif/service/BaseService.java
@@ -0,0 +1,16 @@
+package ir.maktabsharif.service;
+
+import ir.maktabsharif.exception.BusinessException;
+import ir.maktabsharif.model.BaseModel;
+
+public interface BaseService<T extends BaseModel<ID>, ID> {
+    void save(T t);
+
+    void update(T t);
+
+    T findById(ID id) throws BusinessException;
+
+    void delete(T t);
+
+    void validation(T t);
+}
